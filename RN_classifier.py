e88bdb88207cdff8f36cb1fd8c0fa12de7cf77ef
chapa.Mahendra babu <mahindrachapa@gmail.com>
2026-01-23 17:29:13 +0530

    Add files via upload

---
+++ RN_classifier.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import RadiusNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+
+X, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = Pipeline([
+    ("scaler", StandardScaler()),
+    ("rnn", RadiusNeighborsClassifier(radius=1.0, algorithm='ball_tree'))
+])
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
